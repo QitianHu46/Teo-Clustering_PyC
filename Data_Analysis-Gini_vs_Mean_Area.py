168b57547c84eb3f2007121278db82fc24b4fe69
Qitian Hu <jasonhu@uchicago.edu>
2020-04-02 23:09:38 +0800

    update analysis

--- Data_Analysis-Gini_vs_Mean_Area.py
+++ Data_Analysis-Gini_vs_Mean_Area.py
@@ -16,9 +16,7 @@ import seaborn as sns
 import scipy
 
 source_path = 'Export/3-7_Cluster_Statistics/'
-
-
-# %% ########### PLOTTING ########## Define plotting functions: Gini, Plot_Ideal, plot_log_with_stat
+# %% Define plotting functions: Gini, Plot_Ideal, plot_log_with_stat
 def gini(x):
 	# Mean absolute difference
 	mad = np.abs(np.subtract.outer(x, x)).mean()
@@ -107,24 +105,8 @@ def plot_log_with_stat_dense(x, y, file_list, xlabel='x', ylabel='y', title='tit
 	return
 
 
-# %% =========plot cluster characterstics: define the parametres and names=========
-x_y_set = [
-	('building_area', 'Area_gini'),
-	('building_area', 'building_density'),
-	('building_area', 'high'),
-	# ('building_area', 'avg_status'),
-	('total_households', 'cluster_area'),
-	('total_households', 'Area_gini'),
-	('total_households', 'high'),
-	('total_households', 'avg_status'),
-	('Area_mean', 'Area_gini'),
-	('Area_gini', 'building_density'),
-	('Area_gini', 'high'),
-	('Area_gini', 'low'),
-	('Area_gini', 'avg_status'),
-	('high', 'low'),
-	('building_density', 'high')
-]
+# %% Define the parametres and names=========
+
 
 dont_log_cols = ['Unnamed: 0', 'label', 'building_area', 'total_households',
             'total_status', 'num_buildings', 'cluster_area',
@@ -147,33 +129,61 @@ plz = ['Plazas_Over10K_Minus_Sun_Thiessen.csv']
 
 file_list = ms_s + km_s + DB_s + plz
 
-# %% ########## plot HISTOGRAM of household area ##########
-households = gpd.read_file('Export/Total_Households_info.geojson')
-households.Area.value_counts()
-
-# %% pltting using matplotlib
-plt.hist(households[households.Status == 2]['Area'], log=True, bins=50)
-plt.hist(households[households.Status == 1]['Area'], log=True, bins=200, color='red')
-
-plt.hist(households[households.Status == 3]['Area'], log=True, bins=1, color='black')
-plt.title('Counting numbers of households\nRed: high status, blue')
-plt.xlabel('Household Area (sq meter)')
-plt.ylabel('Log Count')
-# plt.savefig('Export/3-7_Cluster_Statistics/relatin_graphs/Histogram-household_area-rich.png')
-plt.savefig('/Users/qitianhu/Desktop/histogram-2.png')
-# plt.show()
-
-# %% ########## Household Area Basic analysis -- graph with ideal Gini line ##########
 hh = gpd.read_file('Export/Total_Households_info.geojson')
-# hh.Area.value_counts()
-# we can reduce the thing to 330, 25, 1200
-dtf = pd.read_csv('Export/3-7_Cluster_Statistics/Kmeans-k=100.csv')[['Area_mean', 'Area_gini']]
+# %% ########## Plot graph with ideal Gini line ##########
+dtf = pd.read_csv('Export/3-7_Cluster_Statistics/Kmeans-k=100.csv')
 dtf.plot.scatter('Area_mean', 'Area_gini')
-a, b, c = hh.Area.min(), hh.Area.mean(), 1250#hh.Area.max()
+a, b, c = hh.Area.min(), hh.Area.mean(), hh.Area.max()
 plt_ideal(a, b)
 plt_ideal(b, c)
 plt.show()
-#%% drawing a randomized case
+# %% 在用 COMPARISON -- real hh data, real cluster size, random grouping by household
+areas = list(hh.Area)
+title = 'Kmeans-k=100'
+dtf = pd.read_csv('Export/3-7_Cluster_Statistics/' + title + '.csv')
+random.shuffle(areas)
+clusters = []
+for n in dtf.num_buildings:
+	clusters.append(areas[:n])
+	areas = areas[n:]
+a, b, c = hh.Area.min(), hh.Area.mean(), hh.Area.max()
+plt_ideal(a, b)
+plt_ideal(b, c)
+# plt_ideal(a, c)
+sns.scatterplot(map(lambda _: sum(_)/len(_), clusters),
+                map(gini, clusters))
+plt.title(title + ' Random Clustering')
+plt.show()
+# %% 不用 COMPARISON -- real hh data, real cluster size, random grouping by building
+"""
+note that one building could be split into several clusters here
+"""
+areas = list(hh.Area)
+num_cluster = 50
+# group areas according to buildings, to avoid households in the same building
+# cluster into different clusters
+i, hh_grouped, tmp = 0, [], []
+while i < len(hh.Area):
+	tmp.append(areas[i])
+	# print(i, end='-')
+	while i < 15686 and areas[i] == areas[i+1]:
+		# print(i, end='-')
+		tmp.append(areas[i+1])
+		i += 1
+	hh_grouped.append(tmp)
+	tmp = []
+	i += 1
+# random.shuffle(hh_grouped)
+clusters = []
+building_per_cluster = int(len(hh_grouped) / num_cluster)
+while len(hh_grouped) > building_per_cluster:
+	tmp = hh_grouped[:building_per_cluster]
+	clusters.append([j for i in tmp for j in i]) # 这个太他妈骚了，牛逼
+	hh_grouped = hh_grouped[building_per_cluster:]
+clusters.append(hh_grouped)
+
+
+# %% COMPARISON -- fake data, random grouping
 import scipy.stats
 
 def my_distribution(min_val, max_val, mean, std):
@@ -191,7 +201,7 @@ def my_distribution(min_val, max_val, mean, std):
         raise ValueError('Cannot create distribution for the given parameters.')
     # Make scaled beta distribution with computed parameters
     return scipy.stats.beta(alpha, beta, scale=scale, loc=location)
-#%%
+# %% create my own household data
 
 md = my_distribution(25, 2015, hh.Area.mean(), 100)
 # x = md.rvs(5000)
@@ -213,20 +223,7 @@ plt_ideal(a, c)
 sns.scatterplot(map(lambda x: sum(x)/len(x), xs), map(gini, xs))
 plt.show()
 
-#%% plot Area_mean vs Area_gini with ideal lines
+#%% Plot Dense Area_mean vs Area_gini with ideal lines
 plot_log_with_stat_dense(x='Area_mean', y='Area_gini', gini_ideal=True, file_list=file_list,
                          save_path='/Users/qitianhu/Desktop/',
                          show_plot=False, do_linear_regres=False)
-
-
-
-
-
-
-
-
-
-
-
-
-
--- Data_Analysis-Visulization.py
+++ Data_Analysis-Visulization.py
@@ -22,6 +22,28 @@ import scipy
 
 source_path = 'Export/3-7_Cluster_Statistics/'
 
+# file_list = [i for i in walk(source_path)][0][2]
+# if '.DS_Store' in file_list:
+# 	file_list.remove('.DS_Store')
+
+file_list = \
+	[
+		'DBSCAN-eps=184.csv',
+		'DBSCAN-eps=141.csv',
+		'DBSCAN-eps=111.5.csv',
+		'Kmeans-k=20.csv',
+		'Kmeans-k=50.csv',
+		'Kmeans-k=100.csv',
+		'MeanShift-bandwidth=560.csv',
+		'MeanShift-bandwidth=350.csv',
+		'MeanShift-bandwidth=265.csv',
+		'Plazas_Over10K_Minus_Sun_Thiessen.csv']
+
+dont_log_cols = ['Unnamed: 0', 'label', 'building_area', 'total_households',
+                 'total_status', 'num_buildings', 'cluster_area',
+                 'Area_gini', 'Area_mean', 'building_density', 'high', 'mid', 'low',
+                 'status_Gini', 'avg_status']
+
 
 # %% ########### PLOTTING ########## Define plotting functions: Gini, Plot_Ideal, plot_log_with_stat
 def gini(x):
@@ -124,7 +146,7 @@ def plot_log_with_stat_dense(x, y, file_list, xlabel='x', ylabel='y', title='tit
 			sns.scatterplot(x=data_x, y=data_y)
 			title_str = file_list[i][:-4] + remove_notice
 		if gini_ideal:
-			a, b, c = 25, 326.29, 2015 # these are min, mean, and max household area
+			a, b, c = 25, 326.29, 2015  # these are min, mean, and max household area
 			plt_ideal(a, b)
 			plt_ideal(b, c)
 		plt.xlabel(x_label)
@@ -143,149 +165,59 @@ def plot_log_with_stat_dense(x, y, file_list, xlabel='x', ylabel='y', title='tit
 	return
 
 
-# %% initialize path
-file_list = [i for i in walk(source_path)][0][2]
-if '.DS_Store' in file_list:
-	file_list.remove('.DS_Store')
+if __name__ == "__main__":
+	# %% =========plot cluster characterstics: define the parametres and names=========
+	x_y_set = [
+		('building_area', 'Area_gini'),
+		('building_area', 'building_density'),
+		('building_area', 'high'),
+		# ('building_area', 'avg_status'),
+		('total_households', 'cluster_area'),
+		('total_households', 'Area_gini'),
+		('total_households', 'high'),
+		('total_households', 'avg_status'),
+		('Area_mean', 'Area_gini'),
+		('Area_gini', 'building_density'),
+		('Area_gini', 'high'),
+		('Area_gini', 'low'),
+		('Area_gini', 'avg_status'),
+		('high', 'low'),
+		('building_density', 'high')
+	]
 
-# %% =========plot cluster characterstics: define the parametres and names=========
-x_y_set = [
-	('building_area', 'Area_gini'),
-	('building_area', 'building_density'),
-	('building_area', 'high'),
-	# ('building_area', 'avg_status'),
-	('total_households', 'cluster_area'),
-	('total_households', 'Area_gini'),
-	('total_households', 'high'),
-	('total_households', 'avg_status'),
-	('Area_mean', 'Area_gini'),
-	('Area_gini', 'building_density'),
-	('Area_gini', 'high'),
-	('Area_gini', 'low'),
-	('Area_gini', 'avg_status'),
-	('high', 'low'),
-	('building_density', 'high')
-]
+	ms_s = ['MeanShift-bandwidth=560.csv',
+	        'MeanShift-bandwidth=350.csv',
+	        'MeanShift-bandwidth=265.csv']
 
-dont_log_cols = ['Unnamed: 0', 'label', 'building_area', 'total_households',
-            'total_status', 'num_buildings', 'cluster_area',
-            'Area_gini', 'Area_mean', 'building_density', 'high', 'mid', 'low',
-            'status_Gini', 'avg_status']
+	km_s = ['Kmeans-k=20.csv',
+	        'Kmeans-k=50.csv',
+	        'Kmeans-k=100.csv']
 
-file_list = \
-	[
-		'DBSCAN-eps=184.csv',
-		'DBSCAN-eps=141.csv',
-		'DBSCAN-eps=111.5.csv',
-		'Kmeans-k=20.csv',
-		'Kmeans-k=50.csv',
-		'Kmeans-k=100.csv',
-		'MeanShift-bandwidth=560.csv',
-		'MeanShift-bandwidth=350.csv',
-		'MeanShift-bandwidth=265.csv',
-		'Plazas_Over10K_Minus_Sun_Thiessen.csv']
+	DB_s = ['DBSCAN-eps=184.csv',
+	        'DBSCAN-eps=141.csv',
+	        'DBSCAN-eps=111.5.csv']
 
-ms_s = ['MeanShift-bandwidth=560.csv',
-        'MeanShift-bandwidth=350.csv',
-        'MeanShift-bandwidth=265.csv']
+	plz = ['Plazas_Over10K_Minus_Sun_Thiessen.csv']
 
-km_s = ['Kmeans-k=20.csv',
-        'Kmeans-k=50.csv',
-        'Kmeans-k=100.csv']
+	# %% Do PLOTTING in congreged manner
+	for pair in x_y_set:
+		var_x = pair[0]
+		var_y = pair[1]
+		plot_log_with_stat_dense(x=var_x, y=var_y, file_list=file_list, show_plot=False)
+		plt.close('all')
 
-DB_s = ['DBSCAN-eps=184.csv',
-        'DBSCAN-eps=141.csv',
-        'DBSCAN-eps=111.5.csv']
+	# %% ########## plot HISTOGRAM of household area ##########
+	households = gpd.read_file('Export/Total_Households_info.geojson')
+	households.Area.value_counts()
 
-plz = ['Plazas_Over10K_Minus_Sun_Thiessen.csv']
+	# %% pltting using matplotlib
+	plt.hist(households[households.Status == 2]['Area'], log=True, bins=50)
+	plt.hist(households[households.Status == 1]['Area'], log=True, bins=200, color='red')
 
-# %% Do PLOTTING in congreged manner
-for pair in x_y_set:
-	var_x = pair[0]
-	var_y = pair[1]
-	plot_log_with_stat_dense(x=var_x, y=var_y, file_list=file_list, show_plot=False)
-	plt.close('all')
-
-# %% ########## plot HISTOGRAM of household area ##########
-households = gpd.read_file('Export/Total_Households_info.geojson')
-households.Area.value_counts()
-
-# %% pltting using matplotlib
-plt.hist(households[households.Status == 2]['Area'], log=True, bins=50)
-plt.hist(households[households.Status == 1]['Area'], log=True, bins=200, color='red')
-
-plt.hist(households[households.Status == 3]['Area'], log=True, bins=1, color='black')
-plt.title('Counting numbers of households\nRed: high status, blue')
-plt.xlabel('Household Area (sq meter)')
-plt.ylabel('Log Count')
-# plt.savefig('Export/3-7_Cluster_Statistics/relatin_graphs/Histogram-household_area-rich.png')
-plt.savefig('/Users/qitianhu/Desktop/histogram-2.png')
+	plt.hist(households[households.Status == 3]['Area'], log=True, bins=1, color='black')
+	plt.title('Counting numbers of households\nRed: high status, blue')
+	plt.xlabel('Household Area (sq meter)')
+	plt.ylabel('Log Count')
+	# plt.savefig('Export/3-7_Cluster_Statistics/relatin_graphs/Histogram-household_area-rich.png')
+	plt.savefig('/Users/qitianhu/Desktop/histogram-2.png')
 # plt.show()
-
-# %% ########## Household Area Basic analysis -- graph with ideal Gini line ##########
-hh = gpd.read_file('Export/Total_Households_info.geojson')
-# hh.Area.value_counts()
-# we can reduce the thing to 330, 25, 1200
-dtf = pd.read_csv('Export/3-7_Cluster_Statistics/Kmeans-k=100.csv')[['Area_mean', 'Area_gini']]
-dtf.plot.scatter('Area_mean', 'Area_gini')
-a, b, c = hh.Area.min(), hh.Area.mean(), 1250#hh.Area.max()
-plt_ideal(a, b)
-plt_ideal(b, c)
-plt.show()
-#%% drawing a randomized case
-import scipy.stats
-
-def my_distribution(min_val, max_val, mean, std):
-    scale = max_val - min_val
-    location = min_val
-    # Mean and standard deviation of the unscaled beta distribution
-    unscaled_mean = (mean - min_val) / scale
-    unscaled_var = (std / scale) ** 2
-    # Computation of alpha and beta can be derived from mean and variance formulas
-    t = unscaled_mean / (1 - unscaled_mean)
-    beta = ((t / unscaled_var) - (t * t) - (2 * t) - 1) / ((t * t * t) + (3 * t * t) + (3 * t) + 1)
-    alpha = beta * t
-    # Not all parameters may produce a valid distribution
-    if alpha <= 0 or beta <= 0:
-        raise ValueError('Cannot create distribution for the given parameters.')
-    # Make scaled beta distribution with computed parameters
-    return scipy.stats.beta(alpha, beta, scale=scale, loc=location)
-#%%
-
-md = my_distribution(25, 2015, hh.Area.mean(), 100)
-# x = md.rvs(5000)
-x = np.array([])
-base = 3000
-x = np.append(x, np.array([25 for i in range(base*30)] +
-                          [326 for i in range(base*50)] +
-                          [340 for i in range(base*25)] +
-                          [310 for i in range(base*25)] +
-                          [2000 for i in range(int(base*2.5))] +
-                          [2030 for i in range(int(base*2.5))] +
-                          [2015 for i in range(base*10)]))
-np.random.shuffle(x)
-xs = np.split(x, 5000)
-a, b, c = hh.Area.min(), hh.Area.mean(), hh.Area.max()
-plt_ideal(a, b)
-plt_ideal(b, c)
-plt_ideal(a, c)
-sns.scatterplot(map(lambda x: sum(x)/len(x), xs), map(gini, xs))
-plt.show()
-
-#%% plot Area_mean vs Area_gini with ideal lines
-plot_log_with_stat_dense(x='Area_mean', y='Area_gini', gini_ideal=True, file_list=file_list,
-                         save_path='/Users/qitianhu/Desktop/',
-                         show_plot=False, do_linear_regres=False)
-
-
-
-
-
-
-
-
-
-
-
-
-
